4c018237a281ac8c6ec8c4e537fb3f4c6c333939
4ntonin <zenoob04@gmail.com>
2021-04-12 18:36:48 +0200

    Update BDD.py
    Modification de la méthode requetesql qui peut maintenant prendre des variables (questions) dans sa commande.

--- BDD.py
+++ BDD.py
@@ -6,8 +6,11 @@ class BDD:
         self.conn = sqlite3.connect(f'{nom}.db')
         self.cur = self.conn.cursor()
 
-    def requetesql(self, commande):
-        self.cur.execute(commande)
+    def requetesql(self, commande, variables=None):
+        if variables:
+            self.cur.execute(commande, variables)
+        else:
+            self.cur.execute(commande)
         self.conn.commit()
 
     def get_tables(self):

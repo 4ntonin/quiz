bed89ebe2fc850f161f10700fb24e1c732ebf8a3
Antonin <72139924+4ntonin@users.noreply.github.com>
2021-04-12 18:55:35 +0200

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import BDD
-
-base = BDD.BDD("test")
-
-base.requetesql("CREATE TABLE IF NOT EXISTS Test1 (id INTEGER, question TEXT, reponse TEXT)")
-base.requetesql("CREATE TABLE IF NOT EXISTS Test2 (id INTEGER, question TEXT, reponse TEXT)")
-base.requetesql("CREATE TABLE IF NOT EXISTS Test3 (id INTEGER, question TEXT, reponse TEXT)")
-print(base.get_tables())

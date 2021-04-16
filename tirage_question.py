b40e3db52d6bb6195408e2d1ded15b60cd97d656
4ntonin <zenoob04@gmail.com>
2021-04-16 19:57:14 +0200

    Update tirage_question.py
    La méthode tire au sort l'id (clé primaire) de la question, cela permet un tri plus facile.

--- tirage_question.py
+++ tirage_question.py
@@ -1,16 +1,23 @@
+"""
+Il faudra utiliser cette classe dans le fichier principal du jeu
+Actuellement, beaucoup de fichiers sont reliés à la base de données 'test',
+il faudra les lier à la base de données principale ('BDDQuiz') lors de
+l'intégration complète du jeu.
+"""
+
 import BDD
 from random import randint
 
 
 class Tirage:
-    def __init__(self):
-        self.base = BDD.BDD("BDDQuiz")
+    def __init__(self, theme):
+        self.base = BDD.BDD("test")
 
-    def get_question(self, theme):
-        self.base.requetesql("SELECT question from {}".format(str(theme)))
-        question = [i[0] for i in self.base.get_cursor().fetchall()]
-        return question
+        self.base.requetesql("SELECT id from {}".format(str(theme)))
+        self.ids = [i[0] for i in self.base.get_cursor().fetchall()]
 
-    def pick_random_question(self, theme):
-        question = self.get_question(theme)
-        return question[randint(0, len(question) - 1)]
+    def pick_random_id(self):
+        i = randint(0, len(self.ids) - 1)
+        picked_id = self.ids[i]
+        self.ids.pop(i)
+        return picked_id

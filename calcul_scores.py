fe569a8492ce9971f0ec366126386e0ab011297b
4ntonin <zenoob04@gmail.com>
2021-04-28 14:28:50 +0200

    Update calcul_scores.py
    Ajout de la méthode valider.

--- calcul_scores.py
+++ calcul_scores.py
@@ -1,19 +1,31 @@
 class calculScores:
     def __init__(self):
+        self.__joueuractif = 0
         self.__scoreJ1 = 0
         self.__scoreJ2 = 0
         self.__scoreJ3 = 0
         self.__scoreJ4 = 0
+    
+    def set_joueur(self, joueur):
+        self.__joueuractif = joueur
+    
+    def get_joueur(self):
+        return self.__joueuractif
 
-    def calcul_score(self, joueur, points):
-        if joueur == 1:
+    def calcul_score(self, points):
+        if self.__joueuractif == 1:
             self.__scoreJ1 += points
-        elif joueur == 2:
+        elif self.__joueuractif == 2:
             self.__scoreJ2 += points
-        elif joueur == 3:
+        elif self.__joueuractif == 3:
             self.__scoreJ3 += points
-        elif joueur == 4:
+        elif self.__joueuractif == 4:
             self.__scoreJ4 += points
 
     def get_scores(self):
         return self.__scoreJ1, self.__scoreJ2, self.__scoreJ3, self.__scoreJ4
+    
+    def valider(self):
+        """Valide la réponse du joueur et appelle calcul_score pour distribuer les points"""
+        joueur = self.get_joueur
+        return self.calcul_score(joueur, 1)
